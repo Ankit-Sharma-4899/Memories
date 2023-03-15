02b75dc5b7d329903cd2001ea982aad28f3805ae
Ankit Sharma <anksh4899@gmail.com>
2023-03-15 14:05:50 +0530

    Code optimization

--- app/src/main/java/com/memories/memories/Adapters/DatesAdapter.kt
+++ app/src/main/java/com/memories/memories/Adapters/DatesAdapter.kt
@@ -5,7 +5,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import java.util.*
 
-class DatesAdapter(private val data: List<Date>) : RecyclerView.Adapter<DatesAdapter.MyViewHolder>() {
+class DatesAdapter(private val data: List<Date>) :
+    RecyclerView.Adapter<DatesAdapter.MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return onCreateViewHolder(parent, viewType)
