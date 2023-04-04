92cfffd0fba087412217af2bd417839d0c563b8a
Ankit Sharma <anksh4899@gmail.com>
2023-04-04 15:32:37 +0530

    Login and Signup using firebase is done.

--- app/src/main/java/com/memories/memories/LoginFragment.kt
+++ app/src/main/java/com/memories/memories/LoginFragment.kt
@@ -5,29 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.memories.memories.databinding.ActivityMainBinding.bind
 import com.memories.memories.databinding.ActivityMainBinding.inflate
 import com.memories.memories.databinding.FragmentLoginBinding
+import com.utils.showToast
 
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
         binding = DataBindingUtil.setContentView(requireActivity(),R.layout.fragment_login)
         Glide.with(this@LoginFragment).asGif().load(R.drawable.second_screen_gif).into(binding.loginanim);
         binding.createone.setOnClickListener{
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
 
         }
-        binding.btnlogin.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_allDatesFragment2)
+        binding.btnlogin.setOnClickListener{
+            login()
         }
 
+
+    }
+    private fun login() {
+        binding. email.text.toString()
+        binding.password.text.toString()
+        // calling signInWithEmailAndPassword(email, pass)
+        // function using Firebase auth object
+        // On successful response Display a Toast
+
+        if (binding.email.text.isBlank() || binding.password.text.isBlank()) {
+            Toast.makeText(requireContext(), "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+        auth.signInWithEmailAndPassword( binding. email.text.toString(), binding.password.text.toString())
+            .addOnCompleteListener(requireActivity()) {
+            if (it.isSuccessful) {
+                (activity as MainActivity).showToast("Login Successfull")
+                findNavController().navigate(R.id.action_loginFragment_to_allDatesFragment2)
+            } else
+                (activity as MainActivity).showToast("Log In failed")
+        }.addOnFailureListener {
+                (activity as MainActivity).showToast("Lcnsicnsjc")
+            }
     }
 
 }
--- app/src/main/java/com/memories/memories/RegisterFragment.kt
+++ app/src/main/java/com/memories/memories/RegisterFragment.kt
@@ -1,26 +1,73 @@
 package com.memories.memories
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.bumptech.glide.util.Util
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.memories.memories.databinding.FragmentRegisterBinding
+import com.utils.showToast
 
 
 class RegisterFragment : Fragment() {
 
 private lateinit var binding: FragmentRegisterBinding
+private lateinit var auth:FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(requireActivity(),R.layout.fragment_register)
-//        Glide.with(this@RegisterFragment).asGif().load(R.drawable.third_screen_gif).into(binding.loginanim);
-//        binding.loginbutton.setOnClickListener {
-//            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
-//        }
-    }
 
+        auth = Firebase.auth
+        binding = DataBindingUtil.setContentView(requireActivity(),R.layout.fragment_register)
+        binding.btnregister.setOnClickListener{
+            signUpUser()
+
+        }
+        Glide.with(this@RegisterFragment).asGif().load(R.drawable.third_screen_gif).into(binding.loginanim);
+
+    }
+    private fun signUpUser() {
+        binding.emailregister.text.toString()
+        binding.passwordregister.text.toString()
+        binding.confirmpass.text.toString()
+
+        // check pass
+        if (binding.emailregister.text.isBlank() || binding.passwordregister.text.isBlank() || binding.confirmpass.text.isBlank()) {
+            Toast.makeText(requireContext(), "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (binding.passwordregister.text.toString() != binding.confirmpass.text.toString()) {
+            (activity as MainActivity).showToast("Password and confirm password do not match")
+            return
+        }
+        // If all credential are correct
+        // We call createUserWithEmailAndPassword
+        // using auth object and pass the
+        // email and pass in it.
+        auth.createUserWithEmailAndPassword(binding.emailregister.text.toString(), binding.passwordregister.text.toString())
+            .addOnCompleteListener(requireActivity()) {
+            if (it.isSuccessful) {
+                (activity as MainActivity).showToast("Login Successfully")
+                findNavController().navigate(R.id.action_registerFragment_to_allDatesFragment2)
+
+            } else {
+                (activity as MainActivity).showToast("Signed in failed")
+                Log.d("hghghb", "$it")
+            }
+        }.addOnFailureListener {
+                (activity as MainActivity).showToast("Please Enter Strong Password")
+            }
+    }
 }
+
